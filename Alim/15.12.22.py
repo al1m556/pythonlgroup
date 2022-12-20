7a8cc64344582d74810bd7df65cd91e5d9755227
al1m556 <revixeer03@mail.ru>
2022-12-20 19:04:22 +0600

    1

--- Alim/15.12.22.py
+++ Alim/15.12.22.py
@@ -1,3 +1,6 @@
+"/start"
+"/help"
+"/hello"
 def create_user_info(user_id,first_name,last_name,is_bot = False):
     user_info = {'user_id':user_id, 
     'user_first_name': first_name,
@@ -5,7 +8,7 @@ def create_user_info(user_id,first_name,last_name,is_bot = False):
     'is_bot': is_bot}
     return user_info
 
-def create_message_info(message_id,text):
+def create_message_info(text):
     message = {
         'message_id': message_id,
         'text': text,
@@ -20,20 +23,22 @@ def create_message(from_user,message):
     full_message.append(message)
     return full_message
 
-message_id = 0 
+message_id = 0
 
 user = create_user_info(1,'Alim','Sansyzbay')
 bot = create_user_info(0,'Test','Bot',True)
 
 def get_message():
-    message_text = input('Введите текст сообщения')
-    message_info = create_message_info(0,message_text)
+    global message_id
+    message_id +=1
+    message_text = input('Введите текст сообщения ')
+    message_info = create_message_info(message_text)
     message = create_message(user,message_info)
-    return message 
+    return message
 
 def send_message(text):
     global message_id
-    message_id +=1
+    message_id +=1   # message_id = message_id+1
     message_info = create_message_info(text)
     message = create_message(bot,message_info)
     print(message)
@@ -42,20 +47,20 @@ def send_message(text):
 def message_handler(message):
     message_text = message[1]['text']
     if message_text == '/start':
-        send_message('бот активирован! Введите /help для справки')
+        send_message('Бот активирован! Введите /help для справки ')
     elif message_text == '/hello':
         name = message[0]['user_first_name']
-        last_name = message[0]['user_last_name']
-        send_text = f'Привет! {name} {last_name}, рад тебя видеть!'
+        last_name =  message[0]['user_last_name']
+        send_text = f'Приветствую! {name} {last_name}, рад тебя видеть!'
         send_message(send_text)
-
+        
     elif message_text == '/help':
-        send_message('доступные команды: \n/start\n/hello\n/help')
+        send_message('Доступные команды: \n/start\n/hello\n/help')
     else:
         send_message('Вы ввели недопустимую команду! Попробуйте /help')
+
 
 
 while True:
 
     message_handler(get_message())
-
